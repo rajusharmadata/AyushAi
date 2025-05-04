47464da1c18130733ee1c9cbca3a6763263d1118
Raju kumar <rajusharmadata123@gmail.com>
2025-05-04 12:10:20 +0530

    Update netlify and vite configuration for client-side routing

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -15,4 +15,13 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
